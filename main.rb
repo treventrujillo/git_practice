27149f20d77f601af42a7bd4368e70b456cf0db6
Treven Trujillo <treventrujillo@gmail.com>
2017-08-16 11:34:05 -0600

    completed git command help program

--- main.rb
+++ main.rb
@@ -1,6 +1,23 @@
-puts "Learning Git"
+def puts_git(cmd)
+  puts `git #{cmd} -h`
+end
 
-first_name = "Treven"
-last_name = "Trujillo"
+def menu
+  puts "1: Enter Git Command"
+  puts "2: Exit"
+  choice = gets.to_i
+  case choice
+    when 1
+      puts "Enter A Git Command"
+      puts_git(gets.strip)
+    when 2
+      puts "Thanks for using the git command"
+      exit
+    else
+      puts "Invalid Choice!"
+      menu
+  end
+  menu
+end
 
-puts "Hello #{first_name} #{last_name}"
+menu
